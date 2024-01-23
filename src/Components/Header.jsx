b13397afe4418996e255d9a364dc9bc39ad4145a
Shinzo27 <20bmiit031@gmail.com>
2024-01-23 23:47:44 +0530

    23-01

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../App.css'
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className='text-3xl font-bold underline'>Header</h1>
     </div>
   )
 }
