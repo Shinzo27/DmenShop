33636ccd3e369583b71e03b13dbc6c5a796bf961
Shinzo27 <20bmiit031@gmail.com>
2024-01-26 16:02:49 +0530

    26-01

--- src/Components/ProductContainer.jsx
+++ src/Components/ProductContainer.jsx
@@ -1,37 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import Product from './Product'
-import SearchBar from './SearchBar'
-import Shimmer from './Shimmer'
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
+import Shimmer from "./Shimmer";
+import { CiSearch } from "react-icons/ci";
 
-const ProductContainer = () => {
-  const [Products, setProducts] = useState([])
-
-  useEffect(()=>{
-    getRestaurants()
-  }, []);
-
-
-  async function getRestaurants() {
-    const data = await fetch(
-        'https://api.escuelajs.co/api/v1/products'
-    );
-
-    const json = await data.json();
-    console.log(json)
-    setProducts(json);
-  }
-
-  return Products.length === 0 ? (<Shimmer/>) : (
-    <div className='flex flex-wrap gap-5 justify-center md: mx-3 mb-10'>
-        {
-          Products.slice(0,20).map((product)=>{
-            return (
-              <Product {...product} key={product.id}/>
-            )
-          })
-        }
-    </div>
+function filterData(searchText, products){
+  const filterData = products.filter( (product) =>
+      product.title.toLowerCase().includes(searchText.toLowerCase())
   )
+  // console.log(filterData)
+  return filterData;
 }
 
-export default ProductContainer
+const ProductContainer = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const data = await fetch("https://api.escuelajs.co/api/v1/products");
+    const json = await data.json();
+    // console.log(json)
+    setAllProducts(json);
+    setFilterProducts(json);
+  }
+
+  return allProducts.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div>
+        <div className="relative my-10 pl-3">
+          <div className="absolute z-2 cursor-pointer inset-y-0 start-0 flex items-center ps-3">
+            <CiSearch
+              className="m-2 bg-purpleshade-400 p-1 w-7 h-7 text-white rounded-lg"
+              onClick={() => {
+                const data = filterData(searchText, allProducts);
+                setFilterProducts(data);
+              }}
+            />
+          </div>
+          <input
+            className="block z-1 lg:w-1/3 md:w-2/4 w-full p-4 ps-12 text-sm outline-none border rounded-lg bg-grayshade-500 border-grayshade-100 placeholde-grayshade-50 text-white focus:ring-purpleshade-400 focus:border-purpleshade-400"
+            placeholder="Search ..." value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center md: mx-3 mb-10">
+        {filterProducts.slice(0, 20).map((product) => {
+          return <Product {...product} key={product.id} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ProductContainer;
--- src/Layout/Body.jsx
+++ src/Layout/Body.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import SearchBar from '../Components/SearchBar'
 import ProductContainer from '../Components/ProductContainer'
 import useOnline from '../Utils/useOnline'
-
+  
 const Body = () => {
 
   const isOnline = useOnline();
@@ -10,7 +9,6 @@ const Body = () => {
   if(!isOnline) return <h1 className=' m-10 text-center text-5xl'>You are offline!🔴</h1>
   return (
     <div>
-      <SearchBar/>
       <ProductContainer/>
     </div>
   )
